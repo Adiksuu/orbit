133239d832214d2cc8553b75d23a2f6b01ffd670
Adiksuu <codeadiksuu@gmail.com>
2026-06-19 20:41:39 +0200

    Include ActivityLog seeding and adjust Issue factory recycling logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ActivityLog;
 use App\Models\Issue;
 use App\Models\Project;
 use App\Models\User;
@@ -17,15 +18,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-//         User::factory()->create([
-//             'name' => 'Test User',
-//             'email' => 'test@example.com',
-//         ]);
-
         $users = User::factory()->count(10)->create();
         $projects = Project::factory()->count(3)->create();
-        Issue::factory()->count(10)->recycle($users, $projects)->create();
+        Issue::factory()->count(10)->recycle([$users, $projects])->create();
+        ActivityLog::factory()->count(20)->recycle([$users, $projects])->create();
     }
 }
