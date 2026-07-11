6f312167b7f28a5a2eb77813313fe35332c48505
Adiksuu <codeadiksuu@gmail.com>
2026-07-11 20:46:21 +0200

    FE-127 Enhance Badge (labels only) component styles with border variants for better visibility

--- resources/js/Components/Atoms/Badge/Badge.tsx
+++ resources/js/Components/Atoms/Badge/Badge.tsx
@@ -4,7 +4,7 @@ import { cva } from 'class-variance-authority';
 import React from 'react';
 
 export const badgeVariants = cva(
-    'inline-flex items-center py-[2px] px-2 rounded-md text-[10px] font-medium whitespace-nowrap transition-colors',
+    'inline-flex items-center py-[2px] px-2 rounded-md text-[10px] font-medium whitespace-nowrap transition-colors border border-solid border-transparent',
     {
         variants: {
             variant: {
@@ -14,12 +14,13 @@ export const badgeVariants = cva(
                 ghost: 'bg-transparent text-zinc-400',
             },
             color: {
-                bug: 'text-[#f44336] bg-[#f44336]/10',
-                feature: 'text-[#2196f3] bg-[#2196f3]/10',
-                performance: 'text-[#9c27b0] bg-[#9c27b0]/10',
-                design: 'text-[#00bcd4] bg-[#00bcd4]/10',
-                ux: 'text-[#009688] bg-[#009688]/10',
-                chore: 'text-[#e91e63] bg-[#e91e63]/10',
+                bug: 'text-[#f44336] bg-[#f44336]/10 border-[#f44336]/20',
+                feature: 'text-[#2196f3] bg-[#2196f3]/10 border-[#2196f3]/20',
+                performance:
+                    'text-[#9c27b0] bg-[#9c27b0]/10 border-[#9c27b0]/20',
+                design: 'text-[#00bcd4] bg-[#00bcd4]/10 border-[#00bcd4]/20',
+                ux: 'text-[#009688] bg-[#009688]/10 border-[#009688]/20',
+                chore: 'text-[#e91e63] bg-[#e91e63]/10 border-[#e91e63]/20',
                 high: 'text-[#f44336]',
                 medium: 'text-[#ff9800]',
                 low: 'text-[#4caf50]',
